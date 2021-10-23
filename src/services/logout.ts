5e36edf67bd061f1dc855e65d382ff63c1f89251
Arnas Bendaravičius <arnas.bendaravicius@hostinger.com>
2021-10-23 20:17:30 +0100

    setup logout endpoint call

--- src/services/logout.ts
+++ src/services/logout.ts
@@ -1,5 +1,20 @@
-const logout = () => {
+import {API, Routes} from '~/constants';
+import getUrl from '../utils/getUrl';
 
+const logout = async () => {
+  try {
+    await fetch(getUrl(API.Logout), {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      }
+    });
+
+    localStorage.removeItem('token');
+    window.location.href = Routes.Login;
+  }
+  catch (error) {
+    // error
+  }
 };
 
 export default logout;
