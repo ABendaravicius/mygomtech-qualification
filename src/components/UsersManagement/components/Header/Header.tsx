7bebbc6d6c681888bb3d5066f71b80e40a7b6d5d
Arnas Bendaravičius <arnas.bendaravicius@hostinger.com>
2021-10-24 03:39:20 +0100

    fix header show correct wrong email count

--- src/components/UsersManagement/components/Header/Header.tsx
+++ src/components/UsersManagement/components/Header/Header.tsx
@@ -1,6 +1,8 @@
 import {FC} from 'react';
 import {IItem} from "~/services/getUserItems";
 import logout from '../../../../services/logout';
+import itemHasReusedEmail from "~/utils/itemHasReusedEmail";
+import itemHasWrongEmail from "~/utils/itemHasWrongEmail";
 
 import './header-style.scss';
 
@@ -10,13 +12,14 @@ interface IHeader {
 }
 
 const Header: FC<IHeader> = ({items, username}) => {
+  const wrongEmails = items.filter(item => itemHasWrongEmail(item) || itemHasReusedEmail(item, items));
 
   return (
-    <div className="header">
-      <div className="user-section">
+    <div className={`header ${wrongEmails.length === 0 && 'success'}`}>
+      <div className='user-section'>
         <button onClick={logout}>{`Logout ${username}`}</button>
       </div>
-      <h1>{`${items.length} Emails are wrong`}</h1>
+      <h1>{`${wrongEmails.length} Emails are wrong`}</h1>
       <span>Email validator to protect your company from bad registrations</span>
     </div>
   )
