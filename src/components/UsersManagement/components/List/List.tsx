1db79ba61ef0588e9fff798106b6ee1fd0931a5b
Arnas Bendaravičius <arnas.bendaravicius@hostinger.com>
2021-10-24 03:01:32 +0100

    fix console error for list not using keys

--- src/components/UsersManagement/components/List/List.tsx
+++ src/components/UsersManagement/components/List/List.tsx
@@ -60,7 +60,7 @@ const List: FC<IList> = ({items}) => (
   <ul className="list">
     {
       items.map((item) => (
-        <li className="item">
+        <li className="item" key={item.id}>
           <ItemIcon name={item.name}/>
           <div>
             <div className="title">
